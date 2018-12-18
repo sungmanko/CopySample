398e0b439fb27ea464a4134e5b2ba28dbad4f361
sungman ko <33561581+sungmanko@users.noreply.github.com>
2018-12-18 17:55:39 +0900

    CopySample
    Just file copy
    
            Dim fileName As String = "\test.jpg"
            Dim fi As New System.IO.FileInfo(Application.StartupPath + fileName)
            Dim copyFile As System.IO.FileInfo = fi.CopyTo("d:\testFolder" + fileName)

---
+++ CopySample/Form1.vb
@@ -0,0 +1,7 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim fileName As String = "\test.jpg"
+        Dim fi As New System.IO.FileInfo(Application.StartupPath + fileName)
+        Dim copyFile As System.IO.FileInfo = fi.CopyTo("d:\testFolder" + fileName)
+    End Sub
+End Class
